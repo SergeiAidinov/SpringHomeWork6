3359c5de521fb672f28c3532caa6d83fd29c609e
SergeiAidinov <incoming34@yandex.ru>
2022-03-08 21:10:44 +0300

    Added link many to many

--- src/main/java/ru/yandex/incoming34/SpringHomeWork6/entities/StudentEntity.java
+++ src/main/java/ru/yandex/incoming34/SpringHomeWork6/entities/StudentEntity.java
@@ -1,20 +1,32 @@
 package ru.yandex.incoming34.SpringHomeWork6.entities;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "students")
 public class StudentEntity {
-	
-	
+
 	@Id
 	@Column(name = "id")
-	Long studentId;
+	private Long studentId;
+	
 	@Column(name = "name")
-	String studentName;
+	private String studentName;
+	
+	@ManyToMany
+	@JoinTable(
+			name = "link_student_course",
+			joinColumns = @JoinColumn(name = "student_id"),
+			inverseJoinColumns = @JoinColumn(name = "course_id"))
+	private List<CourseEntity> courseEntityList;
 	
 	public Long getStudentId() {
 		return studentId;
@@ -27,5 +39,19 @@ public class StudentEntity {
 	public void setStudentName(String studentName) {
 		this.studentName = studentName;
 	}
+	
+	public List<CourseEntity> getCourseEntityList() {
+		return courseEntityList;
+	}
+
+	public void setStudentId(Long studentId) {
+		this.studentId = studentId;
+	}
+
+	public void setCourseEntityList(List<CourseEntity> courseEntityList) {
+		this.courseEntityList = courseEntityList;
+	}
+	
+	
 
 }
