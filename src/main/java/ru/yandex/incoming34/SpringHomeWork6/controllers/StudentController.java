be2e550b680d6c87207a3c7b432835293474fcd2
SergeiAidinov <incoming34@yandex.ru>
2022-03-09 10:44:51 +0300

    Added NamedParameterJdbcTemplate

--- src/main/java/ru/yandex/incoming34/SpringHomeWork6/controllers/StudentController.java
+++ src/main/java/ru/yandex/incoming34/SpringHomeWork6/controllers/StudentController.java
@@ -3,8 +3,11 @@ package ru.yandex.incoming34.SpringHomeWork6.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sun.istack.NotNull;
@@ -29,6 +32,16 @@ public class StudentController {
     Optional<StudentEntity> studentById(@NotNull Long studentId) {
         return studentService.getStudentById(studentId);
     }
+	
+	@DeleteMapping("/delete-student")
+	void deleteStudent(@NotNull Long studentId) {
+		studentService.deleteStudentById(studentId);
+	}
+	
+	@RequestMapping(value = "/unsubscribe-student", method = RequestMethod.DELETE)
+	void unsubscribe (@NotNull Long studentId, @NotNull Long courseId) {
+		studentService.unsubscribeStudent(studentId, courseId);
+	}
 
 
 }
--- src/main/java/ru/yandex/incoming34/SpringHomeWork6/services/StudentService.java
+++ src/main/java/ru/yandex/incoming34/SpringHomeWork6/services/StudentService.java
@@ -1,8 +1,12 @@
 package ru.yandex.incoming34.SpringHomeWork6.services;
 
+import java.util.Map;
 import java.util.Optional;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import ru.yandex.incoming34.SpringHomeWork6.entities.StudentEntity;
@@ -10,17 +14,38 @@ import ru.yandex.incoming34.SpringHomeWork6.repos.StudentRepo;
 
 @Component
 public class StudentService {
-	
+
 	@Autowired
 	StudentRepo studentRepo;
-	
+
+	DataSource dataSource;
+
+	NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+	@Autowired
+	public StudentService(DataSource dataSource) {
+		this.dataSource = dataSource;
+		namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+	}
+
 	public Iterable<StudentEntity> getAllStudents() {
 		return studentRepo.findAllStudents();
 	}
 
 	public Optional<StudentEntity> getStudentById(Long studentId) {
-		// TODO Auto-generated method stub
 		return studentRepo.findStudentById(studentId);
+	}
+
+	public void deleteStudentById(Long studentId) {
+		studentRepo.deleteById(studentId);
+
+	}
+
+	public void unsubscribeStudent(Long studentId, Long courseId) {
+		Map<String, Long> namedParameters = Map.of("studentId", studentId, "courseId", courseId);
+		String sql = "DELETE FROM link_student_course WHERE student_id = :studentId AND course_id = :courseId";
+		namedParameterJdbcTemplate.update(sql, namedParameters);
+
 	}
 
 }
